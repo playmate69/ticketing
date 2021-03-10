b8a924cbad5a5514cd6f7a662be3b44b37347980
playmate69 <kfchotwings@live.nl>
2021-03-10 08:05:07 +0000

    routing

--- inc/controller.php
+++ inc/controller.php
@@ -4,14 +4,16 @@ namespace app;
 
 class Controller {
 
-    public static function handle404() {
-        echo "Bestand niet gevonden";
-        die();
+    public static function res(int $code) {
+
+        $allowed = [ 400, 401, 403];
+
+        return !in_array($code, $allowed) ? header( "HTTP/1.0 500", true, 500 ) : header( "HTTP/1.0 {$code}", true, $code );
     }
 
-    public static function handle( string $path, string $param ) {
+    public static function handle( string $path = "", string $param ) {
 
-        if( !file_exists($path) ) self::handle404();
+        if( !file_exists($path) ) self::res(404);
 
         include $path;
     }
--- index.php
+++ index.php
@@ -5,10 +5,6 @@ require __DIR__ . '/inc/controller.php';
 
 $router = new \Delight\Router\Router();
 
-$router->get('/', function () {
-    echo "home";
-}) && die();
+$router->get('/tickets/:id', [ 'app\Controller', 'handle' ], [ __DIR__ . "/api/tickets.php" ]) && die();
 
-$router->get('/tickets/:id', [ 'Controller', 'handle' ], [ __DIR__ . "/api/tickets.php" ]) && die();
-
-\app\Controller::handle404();
+\app\Controller::res(401);
