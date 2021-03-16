817c64c38ce9f6619481af66c3ca385f43a8914b
playmate69 <kfchotwings@live.nl>
2021-03-16 09:09:40 +0000

    jemoeder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div id="app">
+
+    <div class="container">
+        <nav class="nav flex align--center ">
+            <router-link to="/" class="flex align--center">Home</router-link>
+            <router-link to="/shop" class="flex align--center">Shop</router-link>
+        </nav>
+    </div>
+
     <router-view/>
   </div>
 </template>
+
+<style>
+
+    .nav {
+        background: var(--light-gray)
+    }
+
+    .nav > a {
+        height: 40px;
+        padding: 0 20px;
+    }
+</style>
